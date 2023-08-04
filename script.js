f4c8234c902362de489ee1fa7d7040ec096ec870
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-08-04 14:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const outputDiv = document.getElementById('output');
+
+function getRandomTime(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const promises = Array.from({ length: 5 }, (_, index) => {
+  return new Promise(resolve => {
+    const randomTime = getRandomTime(1000, 5000);
+    setTimeout(() => {
+      resolve(`Promise ${index + 1} resolved after ${randomTime} milliseconds.`);
+    }, randomTime);
+  });
+});
+
+Promise.any(promises)
+  .then(result => {
+    outputDiv.textContent = result;
+  })
+  .catch(error => {
+    console.error(error);
+  });
